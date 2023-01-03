8ff27885e2a1faccee1b617f8fe5847eb474d706
Jeonghyun <kalsollrem@gmail.com>
2023-01-03 15:43:23 +0900

    검색페이지

--- src/main/java/com/project/novelnet/controller/SearchController.java
+++ src/main/java/com/project/novelnet/controller/SearchController.java
@@ -1,12 +1,14 @@
 package com.project.novelnet.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SearchController
 {
-    public String mybook(){
+    @GetMapping("novelnet/search")
+    public String searchPage(){
 
-        return "mybook";
+        return "search";
     }
 }
