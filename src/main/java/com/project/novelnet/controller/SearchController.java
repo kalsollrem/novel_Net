6772de2159db03e056ef6e82d309a4933c7768e7
Jeonghyun <kalsollrem@gmail.com>
2023-01-04 18:47:12 +0900

    .

--- src/main/java/com/project/novelnet/controller/SearchController.java
+++ src/main/java/com/project/novelnet/controller/SearchController.java
@@ -1,11 +1,14 @@
 package com.project.novelnet.controller;
 
+import com.project.novelnet.Vo.NovelVO;
 import com.project.novelnet.repository.SearchMapper;
 import com.project.novelnet.service.ManageService;
 import com.project.novelnet.service.PageingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class SearchController
@@ -24,6 +27,7 @@ public class SearchController
     @GetMapping("novelnet/search")
     public String searchPage(){
 
+        List<NovelVO> novelVOList = searchMapper.getSearchNovelList("","","",0);
         return "search";
     }
 }
