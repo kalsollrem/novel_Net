e89534ec6bf80542e17cdda5d7ab345136b6c1a0
Jeonghyun <kalsollrem@gmail.com>
2023-01-11 21:10:58 +0900

    .

--- src/main/java/com/project/novelnet/controller/SearchController.java
+++ src/main/java/com/project/novelnet/controller/SearchController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
@@ -62,6 +63,51 @@ public class SearchController
         System.out.println("인저가항2:"+keyword +"/"+ newOld +"/"+category+"/"+page);
 
         //검색갯수 검색
+        if (u_num != null){
+
+            //검색갯수 확보
+            int count =1; //sql추가
+            System.out.println("갯수 : " + count);
+
+            //페이징 처리
+            pageingService.setNowPage(page);
+            pageingService.setTotalCount(count);
+
+            int allPage     = pageingService.getAllPage();
+            int nowCase     = pageingService.getNowCase();
+            int allCase     = pageingService.getAllCase();
+            int leftPage    = pageingService.getLeftPage();
+            int rightPage   = pageingService.getRightPage();
+            int displayPage = pageingService.getDisplayPage();
+
+            System.out.println("전채 간격 " + allCase);
+            System.out.println("현재 간격 " + nowCase);
+            System.out.println("전 버튼 " + leftPage);
+            System.out.println("후 버튼 " + rightPage);
+            System.out.println("하단에 나온 페이지 " + displayPage);
+
+            model.addAttribute("allPage", allPage);
+            model.addAttribute("nowCase", nowCase);
+            model.addAttribute("allCase", allCase);
+            model.addAttribute("leftPage", leftPage);
+            model.addAttribute("rightPage", rightPage);
+            model.addAttribute("displayPage", displayPage);
+
+            //시작페이지처리
+            int start = (Integer.parseInt(page)-1)*10;
+
+            //검색(검색조건, 메인태그, 검색태그, 검색카테고리, 검색어, 시작점 순서)
+            List<NovelVO> novelList = searchMapper.getSearchNovelList("n_date","","","title", "", 0);
+            System.out.println(novelList);
+            model.addAttribute("novelList",novelList);
+
+            //년도확인
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            model.addAttribute("year",year);
+
+        }
+
 
         List<NovelVO> novelVOList = searchMapper.getSearchNovelList("n_date","","","title", "", 0);
         return "search";
