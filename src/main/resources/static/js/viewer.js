7d7518a4930a4f49519b4e8959f30cb65d1eb315
Jeonghyun <kalsollrem@gmail.com>
2023-01-25 01:58:00 +0900

    글삭제 기능 JS수정중

--- src/main/resources/static/js/viewer.js
+++ src/main/resources/static/js/viewer.js
@@ -1,14 +1,46 @@
-// $(function(){
-    // // 추천
-    // $(".memo_good").click(function(){
-    //     if(memuSwitch == 0) {
-    //         $(".memo_good").css("color","#FFA048");
-    //         sessionStorage.setItem("R_ZONE", n_num+"/"+m_num);}
-    //     else{
-    //     }
-    // });
+$(function(){
 
-// })
+    //메모옵션박스
+    var memo_OptBox = $('.memo_OptBox');
+    memo_OptBox.css("position", "absolute");
+    memo_OptBox.css("top", Math.max(0, (($(window).height() - login_box.outerHeight()) / 2) + $(window).scrollTop()) + "px");
+    memo_OptBox.css("left", Math.max(0, (($(window).width() - login_box.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+
+    // 글 수정&삭제창
+    $(".write_update_btn>img").click(function(){
+        $('.memo_option').fadeIn(500);
+    });
+
+    // 글삭제
+    $(".memo_delete>img").click(function(){
+        $('.optTypeA').hide();
+        $('.optTypeB').show();
+    });
+
+    // 글삭제
+    // 거절
+    $(".DeleteNoBtn").click(function(){
+        $('.optTypeB').hide();
+        $('.optTypeA').show();
+    });
+    //수락
+    $(".DeleteYesBtn").click(function(){
+        $.ajax({
+            url:'/memoDelete.do',
+            type:'post',
+            data : {"n_num":n_num},
+            success:function(s){
+                if (s == 0)     {alert("삭제 권한이 없습니다.")}
+                else if (s == 1){alert("삭제되었습니다")}
+                else            {alert("삭제에 실패하였습니다.")}
+            },
+            error:function(){
+                alert("로그인되지 않았거나 혹은 서버와의 통신에 문제가 있습니다.");
+            }
+        });
+    });
+
+})
 
 
 
