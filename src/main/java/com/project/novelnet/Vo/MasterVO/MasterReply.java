08a7fb25a7dc285624ec07ff7e9033a88db0f071
Jeonghyun <kalsollrem@gmail.com>
2023-02-18 01:54:55 +0900

    신고된 댓글 관리 기능 제작중

---
+++ src/main/java/com/project/novelnet/Vo/MasterVO/MasterReply.java
@@ -0,0 +1,26 @@
+package com.project.novelnet.Vo.MasterVO;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class MasterReply {
+
+    String rw_num;
+    String r_num;
+    String nick;
+    String r_memo;
+    String r_date;
+    String r_bad;
+    String r_state;
+    String u_num;
+    String wNick;
+    String wU_mail;
+    String U_LEVEL;
+    String wU_num;
+    String n_num;
+    String n_title;
+    String m_num;
+    String m_title;
+}
